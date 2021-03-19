748c5e81454d3bbb38c67bd272d0c42c02a70240
Scottie <scottie.c.f.lee@gmail.com>
2021-03-19 13:08:42 +0100

    added queries 2 and 3

--- sql_queries/02.sql
+++ sql_queries/02.sql
@@ -1,1 +1,3 @@
-
+SELECT T.D_ID , T.P_ID FROM Treats T, Works_In W
+WHERE W.D_ID = T.D_ID and W.C_ID =
+(SELECT C.C_ID FROM Clinic C WHERE C.C_Name = 'Golden Valley Clinic');
--- sql_queries/03.sql
+++ sql_queries/03.sql
@@ -1,1 +1,4 @@
-
+SELECT D.D_ID as ID, D.D_Name as Name FROM Doctor D
+WHERE D.Speciality = 'General practitioner' and
+D.D_ID in(SELECT D_ID FROM Works_In
+WHERE C_ID !=(SELECT C_ID FROM Clinic WHERE C_Address = 'West Virginia Avenue'));
